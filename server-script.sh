e082ca15f76dce8f7ef882ccbbbdcd3ac640cb32
prajwal <prajwalkumatagi07@gmail.com>
2025-08-19 20:38:14 +0530

    done

--- server-script.sh
+++ server-script.sh
@@ -1,1 +1,14 @@
+echo "=====Cpu usage====="
+top -bn1 | grep "Cpu(s)"
 
+echo "=====memory usage====="
+free -m
+
+echo "=====disk space====="
+df -h
+
+echo "=====top 5 cpu process====="
+ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head -n 6
+
+echo "=====top 5 memory process====="
+ps -eo pid,comm,%cpu,%mem --sort=-%mem | head -n 6
